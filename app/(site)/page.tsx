8b3a42c40f6d12cf2b78b371c7f7a649121757d6
Ruisu Eurimonio <pipelinares2912@gmail.com>
2023-07-24 23:59:43 -0500

    button, header, library, sidebar, sidebarItem components ready

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,18 @@
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="text-green-500"> Hello (site) </div>
+    <div className="
+      bg-neutral-900
+      rounded-lg
+      h-full
+      w-full
+      overflow-hidden
+      overflow-y-auto
+    "> 
+      <Header>
+        header
+      </Header>
+    </div>
   )
 }
